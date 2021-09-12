ade7b1ea02d6f906c60ef7c19ff33870ce9ea5c2
paynd <a.kupreychik@gmail.com>
2021-09-12 11:03:21 +0300

    Code cleanup

--- app/src/main/java/name/paynd/android/clientlist/ui/add/AddClientNavigator.kt
+++
@@ -1,6 +0,0 @@
-package name.paynd.android.clientlist.ui.add
-
-interface AddClientNavigator {
-    fun navigateNext()
-    fun navigateBack()
-}
--- app/src/main/java/name/paynd/android/clientlist/ui/add/State.kt
+++
@@ -1,5 +0,0 @@
-package name.paynd.android.clientlist.ui.add
-
-enum class State {
-    WEIGHT, DATE, PHOTO
-}
